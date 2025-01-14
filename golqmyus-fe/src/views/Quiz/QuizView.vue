c65abce184dc605e24eeb9e259d2f24f9449667a
georgiivanov <gogata92@pm.me>
2025-01-14 20:05:01 +0100

    Implement timer for each question

--- golqmyus-fe/src/views/Quiz/QuizView.vue
+++ golqmyus-fe/src/views/Quiz/QuizView.vue
@@ -1,24 +1,34 @@
 <script setup lang="ts">
 import type { Answer, Question } from './types/QuizTypes';
-import { computed, onMounted, ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { getQuestions } from '@/services/quizService'
 import QuestionComponent from './components/QuestionComponent.vue';
+import ProgersBarComponents from "./components/ProgressBarComponent.vue"
 import { checkAnswer } from './composables/checkAnswer';
+import { Button } from 'primevue';
+import { useTimer } from './composables/useTimer';
 
+const TIME_FOR_ANSWER: number = 3;
+
+const { timeLeft, isTimeUp, start, timePercentage } = useTimer();
+
+const quizStarted = ref<boolean>(false);
 const questions = ref<Question[]>([]);
-const currentQuestionIndex = ref(0);
-const timeLeft = ref(0);
-let timeInterval: ReturnType<typeof setInterval>;
+const currentQuestionIndex = ref<number>(0);
 let score: number = 0;
+
 
 onMounted(async () => {
     const data = await getQuestions();
     questions.value = data;
-    timeLeft.value = questions.value[0].timeLimit;
 
 });
 
 const currentQuestion = computed(() => questions.value[currentQuestionIndex.value]);
+
+const startTimer = () => {
+    start(TIME_FOR_ANSWER);
+}
 
 const handleAnswer = (answer: string) => {
 
@@ -29,20 +39,37 @@ const handleAnswer = (answer: string) => {
     }
     currentQuestionIndex.value++;
 
+    if (currentQuestionIndex.value < questions.value.length) {
+        start(TIME_FOR_ANSWER);
+    }
     console.log('Score: ', score);
 }
+
+watch(isTimeUp, (newVal) => {
+    if (newVal) {
+        handleAnswer('');
+    }
+})
 
 </script>
 
 <template>
     <div class="quiz-view max-w-4xl mx-auto mt-8 p-6 bg-white rounded-lg shadow-lg">
-        <h1 class="text-3xl font-bold text-center mb-6">Quiz Time</h1>
+        <h1 v-if="!quizStarted" class="text-3xl font-bold text-center mb-6">Провери колко ти е голям ЮСА</h1>
+
+
+        <div v-if="!quizStarted" class="text-center mt-6">
+            <Button label="Започни" @click="quizStarted = true; startTimer()" />
+        </div>
+
         <!-- Questions Section -->
-        <div v-if="currentQuestion">
+        <div v-if="currentQuestion && quizStarted">
+            <ProgersBarComponents :progress="timePercentage" />
             <QuestionComponent :question="currentQuestion" :index="currentQuestionIndex" @submitAnswer="handleAnswer" />
         </div>
-        <div v-else>
-            <h1>Completed</h1>
+        <div v-if="quizStarted && currentQuestionIndex >= questions.length">
+            <h1 class="text-xl font-semibold text-center mt-6">Твоят ЮС е толкова голям</h1>
+            <p class="text-center mt-4">{{ score }}</p>
         </div>
     </div>
 </template>
---
+++ golqmyus-fe/src/views/Quiz/components/ProgressBarComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+defineProps({
+    progress: {
+        type: Number,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div class="progress-bar-container">
+        <div class="progress-bar" :style="{ width: progress + '%' }"></div>
+    </div>
+</template>
+
+<style scoped>
+.progress-bar-container {
+    width: 100%;
+    height: 20px;
+    background-color: #f3f4f6;
+    border-radius: 10px;
+}
+
+.progress-bar {
+    height: 100%;
+    background-color: #34D398;
+    border-radius: 10px;
+}
+</style>
